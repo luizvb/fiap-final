33ccdc757ee0500c06ee66dc68f7d6af8b41a33c
Luiz Neto <luiz@lvtec.com.br>
2022-11-21 22:54:09 -0300

    fix cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -10,19 +10,23 @@ app.json_encoder = MongoJSONEncoder
 app.url_map.converters['objectid'] = ObjectIdConverter
 
 @app.route("/api/v1/health", methods=['GET'])
+@cross_origin()
 def health():
       print('teste', flush=True)
       return Gallery.ControllerGallery().health(request)
       
 @app.route("/api/v1/images", methods=['POST'])
+@cross_origin()
 def upload_images():
       return jsonify(Gallery.ControllerGallery().upload_image(request))
 
 @app.route("/api/v1/images", methods=['GET'])
+@cross_origin()
 def get_images():
       return jsonify(Gallery.ControllerGallery().get_images(request))
 
 @app.route("/api/v1/images/<id>", methods=['PUT'])
+@cross_origin()
 def approve_image(id):
     return jsonify(Gallery.ControllerGallery().approve_image(id))
 
